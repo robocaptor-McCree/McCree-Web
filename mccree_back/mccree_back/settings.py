d5e465193e9a4f6d4d552eae961e95c1fbe7108a
Hyeon Seong Jeong <sun3850@naver.com>
2021-08-20 20:06:41 +0900

    connection DB

--- mccree_back/mccree_back/settings.py
+++ mccree_back/mccree_back/settings.py
@@ -73,11 +73,22 @@ WSGI_APPLICATION = 'mccree_back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = { 
+	'default': { 
+    	'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'McCree', 
+        'USER': 'root', 
+        'PASSWORD': 'mccree123', 
+        'HOST': 'localhost', 
+        'PORT': '3306', 
+     } 
 }
 
 
