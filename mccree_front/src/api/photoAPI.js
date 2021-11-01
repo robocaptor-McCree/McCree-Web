5f9504fc1816fbed19bb2af5c9c42efc09505849
juheesvt <kangjuhee18@kookmin.ac.kr>
2021-11-01 21:59:14 +0900

    rename: getphoto -> getPhoto

--- mccree_front/src/api/photoAPI.js
+++ mccree_front/src/api/photoAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default{
-    getphoto(id){
+    getPhoto(id){
         return axios.get(`http://127.0.0.1:8000/api/question/question/${id}/`)
     },
     getQustion(id){
